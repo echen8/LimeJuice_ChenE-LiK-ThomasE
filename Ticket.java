1e46fe0affd550c74ad17a112302a1c5a7d590c2
Eric <echen8@stuy.edu>
2017-04-21 10:50:45 -0400

    editted Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -5,24 +5,94 @@
 
 public class Ticket {
 
-    private int priority;
-    private String problem;
-    private String username;
-    private int id;
-    private boolean resolved;
-    private String solution;
+    private int _priority;
+    private String _problem;
+    private String _name;
+    private int _id;
+    private boolean _resolved;
+    private String _solution;
 
     public Ticket() {
-	priority = 0;
+	_priority = 0;
     }
+
+    public Ticket( int priority, String problem, String name, int id ) {
+	_priority = priority;
+	_problem = problem;
+	_name = name;
+	_id = id;
+    }
+
+    // ACCESSORS ============================================
 
     public int getPriority() {
-	return priority;
+	return _priority;
+    }
+    public String getProblem() {
+	return _problem;
+    }
+    public String getName() {
+	return _name;
+    }
+    public int getID() {
+	return _id;
+    }
+    public boolean isResolved() {
+	return _resolved;
+    }
+    public String getSolution() {
+	return _solution;
     }
 
+    // MUTATORS =============================================
+
+    public int setPriority( int priority ) {
+	int oldPriority = _priority;
+	_priority = priority;
+	return oldPriority;
+    }
+    public String setProblem( String problem ) {
+	String oldProblem = _problem;
+	_problem = problem;
+	return oldProblem;
+    }
+    public String setName( String name ) {
+	String oldName = _name;
+	_name = name;
+	return oldName;
+    }
+    public int setID( int id ) {
+        int oldID = _id;
+	_id = id;
+	return oldID;
+    }
+    public boolean setResolved( boolean resolved ) {
+        boolean oldResolved = _resolved;
+	_resolved = resolved;
+	return oldResolved;
+    }
+    public String setSolution( String solution ) {
+        String oldSolution = _solution;
+	_solution = solution;
+	return oldSolution;
+    }
+
+    // TOSTRING METHOD =======================================
+
+    public String toString() {
+	String retStr = "";
+	retStr += "\nTicket No. " + _id;
+	retStr += "\nName: " + _name;
+	retStr += "\nProblem: " + _problem;
+	retStr += "\nResolved: " + _resolved;
+	return retStr;
+    }
+
+
+
     public int compareTo( Ticket t ) {
-	if ( priority > t.getPriority() ) return 1;
-	else if ( priority == t.getPriority() ) return 0;
+	if ( _priority > t.getPriority() ) return 1;
+	else if ( _priority == t.getPriority() ) return 0;
 	else return -1;
     }
 
